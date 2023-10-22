d75b3a487f9abb857c3a3be4efcfdb18118778c3
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-22 20:18:23 +0300

    onclick cards

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -3,6 +3,7 @@
 import { Card } from "@/components/ui/card"
 import { cn } from "@/lib/utils"
 import { ArrowRight, CodeIcon, ImageIcon, MessageSquare, Music, Video } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 const tools = [
   {
@@ -44,6 +45,7 @@ const tools = [
 
 
 const DashboardPage = () => {
+  const router = useRouter()
   return (
     <div>
       <div className="mb-8 space-y-4">
@@ -57,6 +59,7 @@ const DashboardPage = () => {
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
         {tools.map((tool) => (
           <Card
+          onClick={() => router.push(tool.href)}
             key={tool.href}
             className="p-4 border-black/5 flex items-center 
         justify-between hover:shadow-md transition cursor-pointer"
